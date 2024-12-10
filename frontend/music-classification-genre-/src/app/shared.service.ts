cba2b83ff7cc09a482ef116b18d9328f08a93429
ManarSASSI <manarsassi223@gmail.com>
2024-12-10 19:17:52 +0100

    first commit

---
+++ frontend/music-classification-genre-/src/app/shared.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+
+  constructor(private http: HttpClient) { }
+    uploadFile(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file); // Ajoute le fichier au FormData
+
+    return this.http.post('http://127.0.0.1:5001/predict', formData); // Requête POST
+  }
+    uploadFileSVM(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file); // Ajoute le fichier au FormData
+
+    return this.http.post('http://127.0.0.1:5000/predict', formData); // Requête POST
+  }
+}
